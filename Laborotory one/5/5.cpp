fd8186d9e065adfe7e2715a18b278e3f4bbf4285
Mentor <dm.minaev03@gmail.com>
2020-10-06 22:07:23 +0300

    Create 5.cpp

---
+++ Laborotory one/5/5.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+
+int main()
+{
+	system("chcp 1251");
+	system("cls");
+	
+	
+	int a, b;
+	printf("Напиши своё число: ");
+	scanf_s("%d", &a);
+	system("cls");
+    b = a + a/2;
+	printf("Твоё число: %d\n Число ПК: %d\n Ты проиграл попробуй ещё раз", a, b);
+
+
+}
