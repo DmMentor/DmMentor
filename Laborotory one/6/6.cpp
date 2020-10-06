4c3fbc1d7ab36fddf81f2645ee4c7f31379b9c60
Mentor <dm.minaev03@gmail.com>
2020-10-06 22:07:28 +0300

    Create 6.cpp

---
+++ Laborotory one/6/6.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int main()
+{
+	
+	system("chcp 1251");
+	system("cls");
+	
+	double a, s, d, f, g, h;
+	a = 60;
+	s = 60;
+	d = 24;
+	f = 365;
+	
+	printf("В скольки годах ты хочешь узнать сколько секунд?-");
+	scanf_s("%lf", &g);
+	h = a * s * d * f * g;
+	
+	printf("Ответ: %lf", h);
+	
+}
+
