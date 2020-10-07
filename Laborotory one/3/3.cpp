ac1037deea443b82edf0b361ec2143f4ce38e159
Mentor <dm.minaev03@gmail.com>
2020-10-07 23:58:49 +0300

    Create 3.cpp

---
+++ Laborotory one/3/3.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>;
+
+int main()
+{
+	system("chcp 1251");
+	system("cls");
+	
+	double a, b, c,d;
+	printf("Напиши число которое хочешь получить в 8 степени:");
+	scanf_s("%lf", &a);
+    system("cls");
+	
+	b = a * a;
+	c = b * b;
+	d = c * c;
+	printf("Получило: %lf", d);
+
+
+
+}
