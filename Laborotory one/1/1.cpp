83df1286823c79dc1742d70b7d4851f822922dcf
Mentor <dm.minaev03@gmail.com>
2020-10-11 23:17:06 +0300

    Create 1.cpp

---
+++ Laborotory one/1/1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main()
+{
+	printf("|-------------| |-------------|\n");
+	
+	printf("| Integer: %d; | ", sizeof(int) );     
+	printf("| Short: %d;   | \n", sizeof(short));  
+	printf("| Long: %d;    | ", sizeof(long));       
+	printf("| Char: %d;    | \n", sizeof(char));     
+	printf("| Float: %d;   | ", sizeof(float));     
+	printf("| Double: %d;  | \n", sizeof(double)); 
+
+	printf("|-------------| |-------------|");
+
+}
